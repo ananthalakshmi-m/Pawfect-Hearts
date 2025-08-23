34d004f8c54ac17f2b5020d7a5642b5f50bdf81e
ananthalakshmi-m <malakshmi0903@gmail.com>
2025-08-24 01:00:08 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -166,8 +166,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
