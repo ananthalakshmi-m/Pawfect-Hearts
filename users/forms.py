8d45628d6a64e25cb866686e28972477f791185f
ananthalakshmi-m <malakshmi0903@gmail.com>
2025-10-03 01:16:59 +0530

    Add Forgot password

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.conf import settings
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,4 +15,39 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+    
+class SendGridPasswordResetForm(PasswordResetForm):
+    def send_mail(self, subject_template_name, email_template_name,
+                  context, from_email, to_email, html_email_template_name=None):
+        """
+        Sends password reset email via SendGrid instead of SMTP.
+        """
+        reset_url = f"{context['protocol']}://{context['domain']}/users/reset/{context['uid']}/{context['token']}/"
+
+        html_message = f"""
+        <html>
+            <body style="font-family: Arial, sans-serif; color: #333;">
+                <p>Hello,</p>
+                <p>You requested a password reset for your Pawfect Hearts account.</p>
+                <p>Click the link below to reset your password:</p>
+                <p><a href="{reset_url}">{reset_url}</a></p>
+                <p>If you didn’t request this, you can ignore this email safely.</p>
+                <p style="color: #d63384;">With love,<br>Team Pawfect Hearts 🐶❤️</p>
+            </body>
+        </html>
+        """
+
+        message = Mail(
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to_emails=to_email,
+            subject="🐾 Pawfect Hearts Password Reset",
+            html_content=html_message
+        )
+        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+        sg.send(message)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import signup
+from .forms import SendGridPasswordResetForm
 
 urlpatterns = [
     path("signup/", signup, name="signup"),
@@ -8,5 +9,38 @@ urlpatterns = [
         "accounts/login/",
         auth_views.LoginView.as_view(redirect_authenticated_user=True),
         name="login"
+    ),
+    
+     # Password reset URLs
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html",
+            form_class=SendGridPasswordResetForm,
+            success_url="/users/password_reset_done/"
+        ),
+        name="password_reset"
+    ),
+    path(
+        "password_reset_done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done"
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html",
+            success_url="/users/password_reset_complete/"
+        ),
+        name="password_reset_confirm"
+    ),
+    path(
+        "password_reset_complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete"
     ),
 ]
